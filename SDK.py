7a31041937135b07f022ba0c60ed1b8ce4f13f05
dassowmd <dassowmd@gmail.com>
2017-04-30 23:50:02 -0400

    Got posting flattener working so that it now pivots the data.

--- SDK.py
+++ SDK.py
@@ -3,15 +3,17 @@ from requests.auth import HTTPBasicAuth
 import json
 import csv
 from multiprocessing.pool import ThreadPool
+import pandas as pd
+import datetime
 
 def getJSONDetail(url):
     try:
         r = requests.get(url, auth=HTTPBasicAuth('user', 'craigslist'))
-        print url
+        print str(datetime.now) +  url
         return json.loads(r.content)
     except Exception as e:
         print str(e)
-        print("Something went wrong. You sure you did it right?")
+        # print("Something went wrong. You sure you did it right?")
 
 
 def getCl_Item_IDList(url):
@@ -35,19 +37,27 @@ def getAllDetails(url):
 
     # Create list of URLs to query
     URL_list = []
-    # for i in range(1, count/100):
-    for i in range(1, 5):
+    for i in range(1, count/100):
+    # for i in range(1, 10):
         url = 'http://108.59.217.3:54321/api/listings/?format=json&limit=100&offset=' + str(i * 100)
         URL_list.append(url)
 
-   # Iterate through URL list
+   # Iterate through U+
+    # RL list
+    Items = pd.DataFrame()
     pool = ThreadPool(40)
-    pool.map(getJSONDetail, URL_list)
-    Items = []
-    while url != None:
-        JSON_Results = getJSONDetail(url)
-        Items.append(JSON_Results['results'])
-        url = JSON_Results['next']
+    results = pool.map(getJSONDetail, URL_list)
+    for i in results:
+        for r in i['results']:
+            Items = Items.append(r, ignore_index=True)
+    pool.close()
+    pool.join()
+
+
+    # while url != None:
+    #     JSON_Results = getJSONDetail(url)
+    #     Items.append(JSON_Results['results'])
+    #     url = JSON_Results['next']
 
     return Items
 
@@ -60,11 +70,10 @@ def getCount(url):
 # print(json_data)
 url = 'http://108.59.217.3:54321/api/listings/?format=json&limit=100'
 results = getAllDetails(url)
-saveFilePath = raw_input("Where would you like to save the file? (Include full file path. File name will be CraigslistPosting.csv) \n")
+saveFilePath = 'C:\Users\dasso\OneDrive\Documents\GitHub\Craigslist'
 fullPath = saveFilePath + '/CraigslistPosting.csv'
-with open(fullPath, 'wb') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+print "Saving results to %s" % fullPath
+results.to_csv(fullPath)
 print results
 
 
